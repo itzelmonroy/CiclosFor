79cf79eb17c3ef0bce96c00a9d1aa57f13c80c6c
Alumno <Alumno@PC17>
2019-08-22 09:15:39 -0500

    Ciclo for de cuanto en cuanto

---
+++ CiclosFor/CiclosFor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CiclosFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            Console.WriteLine(" Imprimir los numeros de la x hasta la y");
+            Console.WriteLine("Ingrese su primer valor");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese su segundo valor"); 
+            y= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De cuanto en cuanto irà?");
+            z= Convert.ToInt32(Console.ReadLine());
+
+            if (x>y)
+            {
+                for (int i=x;i>y;i-=z)
+                {
+                    Console.WriteLine("i=" + i);
+                }
+                
+            }
+            else if (x < y)
+            {
+                for (int i = x; i<=y; i += z)
+                {
+                    Console.WriteLine("i=" + i);
+                }
+            }
+
+        }
+    }
+}
